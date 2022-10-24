3e4e13930a656f6151bc59252acb1d69a59dbe2d
Spencer Rafada <spencer.byui@gmail.com>
2022-10-24 10:21:23 -0400

    Initial Commit

---
+++ Flowdrate/FlowdrateApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowdrateApp.swift
+//  Flowdrate
+//
+//  Created by Spencer Rafada on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct FlowdrateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
